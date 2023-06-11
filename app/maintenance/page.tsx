f02cb68cbbf53fad43ff3fbb8bf359478152de51
takumibv <everybody.say.hey.yo@gmail.com>
2023-06-11 14:28:21 +0900

    フォルダ構成修正

---
+++ app/maintenance/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from '@/public/next.svg';
+
+export default function Maintenance() {
+  return (
+    <main className="p-24">
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert w-20"
+        src={logo}
+        alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+
+      <h1 className="text-2xl font-bold mt-2">Maintenance</h1>
+
+      <div className="mt-2">
+        <Link href="/" className="underline">
+          Top
+        </Link>
+      </div>
+    </main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from '@/public/next.svg';
 
 export default function Home() {
   return (
     <main className="p-24">
       <Image
         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="/next.svg"
+        src={logo}
         alt="Next.js Logo"
+        width={180}
+        height={37}
+        priority
+      />
+
+      <Image
+        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+        src="/next.svg"
+        alt="この画像はメンテナンスモードの時には表示できない"
         width={180}
         height={37}
         priority
