91a22fb3fd39fb15771ffa9acdfb7a1c3296d705
takumibv <everybody.say.hey.yo@gmail.com>
2023-06-11 15:01:36 +0900

    メンテナンスのリダイレクト処理 修正

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from "next/server";
 const maintenanceExclusionPaths = ["/"];
 
 const isMaintenanceMode = Boolean(process.env.NEXT_PUBLIC_MAINTENANCE_MODE);
-const allowedIps = process.env.NEXT_PUBLIC_MAINTENANCE_WHITELIST?.split(',') ?? [];
+const maintenanceWhiteListIPs = process.env.NEXT_PUBLIC_MAINTENANCE_WHITELIST?.split(",") ?? [];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
@@ -13,10 +13,19 @@ export function middleware(request: NextRequest) {
 
   // メンテナンスモードの対象かどうか
   const isMaintenanceTargetPath = !maintenanceExclusionPaths.some((path) => pathname === path);
-  const isMaintenanceTargetIP = !allowedIps.includes(ip);
+  const isMaintenanceTargetIP = !maintenanceWhiteListIPs.includes(ip);
 
   if (isMaintenanceMode && isMaintenanceTargetPath && isMaintenanceTargetIP) {
-    request.nextUrl.pathname = `/maintenance`;
+    // 繰り返しリダイレクトを防ぐ
+    if (request.nextUrl.pathname === "/maintenance") return;
+
+    // メンテナンス画面へリダイレクト
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_HOST}/maintenance`);
+  }
+
+  // メンテナンスモードでない時は、/maintenance を404にする
+  if (!isMaintenanceMode && request.nextUrl.pathname === "/maintenance") {
+    request.nextUrl.pathname = `/404`;
     return NextResponse.rewrite(request.nextUrl);
   }
 }
@@ -29,9 +38,9 @@ export const config = {
 };
 
 // IPアドレスを取得する
-function getIP (request: NextRequest) {
+function getIP(request: NextRequest) {
   // x-forwarded-for ヘッダーからIPを取得する
-  const xff = request.headers.get('x-forwarded-for');
+  const xff = request.headers.get("x-forwarded-for");
 
-  return xff ? (Array.isArray(xff) ? xff[0] : xff.split(',')[0]) : '127.0.0.1';
-};
+  return xff ? (Array.isArray(xff) ? xff[0] : xff.split(",")[0]) : "127.0.0.1";
+}
